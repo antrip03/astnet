b9b02fd28311a19743afe041a1db974979b30831
antrip03 <anshultripathi002@gmail.com>
2026-01-07 23:17:22 +0530

    Update test.py

--- ASTNet/test.py
+++ ASTNet/test.py
@@ -1,6 +1,8 @@
 import pprint
 import argparse
 import tqdm
+import os
+import csv
 
 import torch
 import torch.nn as nn
@@ -15,23 +17,21 @@ from models.wresnet2048_multiscale_cattn_tsmplus_layer6 import ASTNet as get_net
 import torch.multiprocessing
 torch.multiprocessing.set_sharing_strategy('file_system')
 
-# --cfg experiments/sha/sha_wresnet.yaml --model-file output/shanghai/sha_wresnet/shanghai.pth GPUS [3]
-# --cfg experiments/ped2/ped2_wresnet.yaml --model-file output/ped2/ped2_wresnet/ped2.pth GPUS [3]
+
 def parse_args():
     parser = argparse.ArgumentParser(description='Test Anomaly Detection')
 
     parser.add_argument('--cfg', help='experiment configuration filename',
-                        default='config/shanghaitech_wresnet.yaml', type=str)
+                        default='config/avenue_kaggle.yaml', type=str)
     parser.add_argument('--model-file', help='model parameters',
-                        default='pretrained/shanghaitech.pth', type=str)
+                        required=True, type=str)
 
     parser.add_argument('opts',
                         help="Modify config options using the command-line",
                         default=None,
                         nargs=argparse.REMAINDER)
 
-    args = parser.parse_args()
-    return args
+    return parser.parse_args()
 
 
 def main():
@@ -49,77 +49,107 @@ def main():
     cudnn.enabled = config.CUDNN.ENABLED
 
     config.defrost()
-    config.MODEL.INIT_WEIGHTS = False       # TODO ? False
+    config.MODEL.INIT_WEIGHTS = False
     config.freeze()
 
-    gpus = [(config.GPUS[0])]
-    # model = models.get_net(config)
+    gpus = [config.GPUS[0]]
+
     if config.DATASET.DATASET == "ped2":
         model = get_net1(config, pretrained=False)
     else:
         model = get_net2(config, pretrained=False)
-    logger.info('Model: {}'.format(model.get_name()))
-    model = nn.DataParallel(model, device_ids=gpus).cuda(device=gpus[0])
-    logger.info('Epoch: '.format(args.model_file))
 
-    # load model
+    logger.info(f'Model: {model.get_name()}')
+    model = nn.DataParallel(model, device_ids=gpus).cuda(device=gpus[0])
+
+    # Load pretrained model
     state_dict = torch.load(args.model_file)
-    if 'state_dict' in state_dict.keys():
-        state_dict = state_dict['state_dict']
-        model.load_state_dict(state_dict)
+    if 'state_dict' in state_dict:
+        model.load_state_dict(state_dict['state_dict'])
     else:
         model.module.load_state_dict(state_dict)
 
-    test_dataset = eval('datasets.get_test_data')(config)
-
+    # Load test dataset
+    test_dataset = datasets.get_test_data(config)
     test_loader = torch.utils.data.DataLoader(
         test_dataset,
-        batch_size=config.TEST.BATCH_SIZE_PER_GPU * len(gpus),
+        batch_size=1,
         shuffle=False,
         num_workers=config.WORKERS,
         pin_memory=True
     )
 
+    # Load ground truth (still used for AUC logging)
     mat_loader = datasets.get_label(config)
     mat = mat_loader()
 
-    psnr_list = inference(config, test_loader, model)
-    assert len(psnr_list) == len(mat), f'Ground truth has {len(mat)} videos, BUT got {len(psnr_list)} detected videos!'
+    psnr_list, frame_paths = inference(config, test_loader, model)
 
-    auc, fpr, tpr = anomaly_util.calculate_auc(config, psnr_list, mat)
+    assert len(psnr_list) == len(mat), \
+        f'GT videos: {len(mat)}, detected videos: {len(psnr_list)}'
 
-    logger.info(f'AUC: {auc * 100:.1f}%')
+    auc, _, _ = anomaly_util.calculate_auc(config, psnr_list, mat)
+    logger.info(f'AUC: {auc * 100:.2f}%')
+
+    # ===== KAGGLE CSV EXPORT =====
+    export_kaggle_csv(psnr_list, frame_paths)
 
 
 def inference(config, data_loader, model):
-    loss_func_mse = nn.MSELoss(reduction='none')
-
+    loss_func = nn.MSELoss(reduction='none')
     model.eval()
+
     psnr_list = []
+    frame_paths = []
+
     ef = config.MODEL.ENCODED_FRAMES
     df = config.MODEL.DECODED_FRAMES
-    fp = ef + df  # number of frames to process
+    fp = ef + df
+
     with torch.no_grad():
         for i, data in enumerate(data_loader):
-            print('[{}/{}]'.format(i+1, len(data_loader)))
+            print(f'[Video {i+1}/{len(data_loader)}]')
             psnr_video = []
 
-            # compute the output
             video, video_name = train_util.decode_input(input=data, train=False)
-            video = [frame.to(device=config.GPUS[0]) for frame in video]
+            video = [f.cuda(config.GPUS[0]) for f in video]
+
+            frame_paths.append(video_name[0])
+
             for f in tqdm.tqdm(range(len(video) - fp)):
                 inputs = video[f:f + fp]
                 output = model(inputs)
                 target = video[f + fp:f + fp + 1][0]
 
-                # compute PSNR for each frame
-                # https://github.com/cvlab-yonsei/MNAD/blob/d6d1e446e0ed80765b100d92e24f5ab472d27cc3/utils.py#L20
-                mse_imgs = torch.mean(loss_func_mse((output[0] + 1) / 2, (target[0] + 1) / 2)).item()
-                psnr = anomaly_util.psnr_park(mse_imgs)
+                mse = torch.mean(
+                    loss_func((output[0] + 1) / 2, (target[0] + 1) / 2)
+                ).item()
+
+                psnr = anomaly_util.psnr_park(mse)
                 psnr_video.append(psnr)
 
             psnr_list.append(psnr_video)
-    return psnr_list
+
+    return psnr_list, frame_paths
+
+
+def export_kaggle_csv(psnr_list, frame_paths):
+    rows = []
+
+    for vid, (scores, frames) in enumerate(zip(psnr_list, frame_paths), start=1):
+        scores = torch.tensor(scores)
+        scores = (scores - scores.min()) / (scores.max() - scores.min() + 1e-8)
+        anomaly_scores = 1 - scores  # invert PSNR
+
+        for idx, s in enumerate(anomaly_scores, start=1):
+            rows.append([f"{vid}_{idx}", float(s)])
+
+    with open("submission_astnet.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["Id", "Predicted"])
+        writer.writerows(rows)
+
+    print("✅ submission_astnet.csv generated")
 
 
 if __name__ == '__main__':
